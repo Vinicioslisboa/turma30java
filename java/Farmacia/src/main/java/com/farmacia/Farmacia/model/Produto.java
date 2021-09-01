d09851162ba36b576af8f53fcb580e1ff5f3ced0
Vinícios Lisboa <developerlisboa@gmail.com>
2021-09-01 15:26:16 -0300

    Crud farmacia

---
+++ java/Farmacia/src/main/java/com/farmacia/Farmacia/model/Produto.java
@@ -0,0 +1,81 @@
+package com.farmacia.Farmacia.model;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name = "tb_produto")
+public class Produto {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+
+	@NotBlank
+	@Size(max = 30)
+	private String nome;
+
+	@NotBlank
+	@Size(max = 50)
+	private String tratamento;
+
+	@NotBlank
+	private BigDecimal preco;
+
+	@ManyToOne
+	@JsonIgnoreProperties("produto")
+	private Categoria categoria;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getTratamento() {
+		return tratamento;
+	}
+
+	public void setTratamento(String tratamento) {
+		this.tratamento = tratamento;
+	}
+
+	public BigDecimal getPreco() {
+		return preco;
+	}
+
+	public void setPreco(BigDecimal preco) {
+		this.preco = preco;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+	
+	
+
+}
